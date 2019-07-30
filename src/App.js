efaae797c30385f56db9cd27dbeebb334eaa1926
vinodh1988 <k.c.vinodh@hotmail.com>
2019-07-30 17:29:16 +0530

    State Demo

--- src/App.js
+++ src/App.js
@@ -4,24 +4,30 @@ import Box from './components/second';
 import './App.css';
 
 class App extends React.Component{
+     constructor(props){
+       super(props);
+       this.state={
+           appname:"ReactJS application- ABCSoft",
+           developers:["Rahul","Ramu","Kiran","Johnson"],
+           childlist:[["Intelli J","SQL Developer","VS Code"],
+                       ["React JS","Java and Spring","SQL"],
+                       ["MySQL","MongoDB"]]
+       }
+     }
      render(){
-       let appname="ReactJS application- ABCSoft";
-       let developers=["Rahul","Ramu","Kiran","Johnson"];
-       let childlist=[["Intelli J","SQL Developer","VS Code"],
-                      ["React JS","Java and Spring","SQL"],
-                      ["MySQL","MongoDB"]];
-      let code= developers.map(x=><li>{x}</li>)
+       
+      let code= this.state.developers.map(x=><li>{x}</li>)
         return(
           <div>
                  <h1> React JS component is working...!!!! </h1>
-                 <h4> {appname}</h4>
+                 <h4> {this.state.appname}</h4>
                  <h4><u>Developers</u></h4>
                  <ul>
                    {code}
                  </ul>
-                 <Box title="Tools" items={childlist[0]}></Box>
-                 <Box title="Technologies" items={childlist[1]}></Box>
-                 <Box title="Databases" items={childlist[2]}></Box>
+                 <Box title="Tools" items={this.state.childlist[0]}></Box>
+                 <Box title="Technologies" items={this.state.childlist[1]}></Box>
+                 <Box title="Databases" items={this.state.childlist[2]}></Box>
                  
                  
           </div>
