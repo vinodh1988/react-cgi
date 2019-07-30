225b8e36850ed31c6d33227b8bb7226970345ab9
vinodh1988 <k.c.vinodh@hotmail.com>
2019-07-30 18:31:58 +0530

    Life cycle n form handling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import logo from './logo.svg';
 import Box from './components/second';
 import './App.css';
@@ -13,10 +14,26 @@ class App extends React.Component{
                        ["React JS","Java and Spring","SQL"],
                        ["MySQL","MongoDB"]]
        }
+
+       this.handleStore=this.handleStore.bind(this);
+       console.log("APP Component constructor");
+     }
+
+     handleStore(){
+        let item=ReactDOM.findDOMNode(this.refs.itemname).value;
+        let type=Number(ReactDOM.findDOMNode(this.refs.type).value);
+        this.state.childlist[type].push(item);
+        this.setState(this.state);
+        
+     }
+     componentWillMount(){
+       console.log("First Rendering is going to happen");
      }
      render(){
+
+      console.log("APP component rendering..!!!")
        
-      let code= this.state.developers.map(x=><li>{x}</li>)
+      let code= this.state.developers.map(x=><li key={x}>{x}</li>)
         return(
           <div>
                  <h1> React JS component is working...!!!! </h1>
@@ -25,14 +42,33 @@ class App extends React.Component{
                  <ul>
                    {code}
                  </ul>
+                 <div id="form">
+                    Item <input type="name" ref="itemname"/>
+                    <select ref="type">
+                      <option value="0">tools</option>
+                      <option value="1">technologies</option>
+                      <option value="2">databases</option>
+                    </select>
+                    <button 
+                    style={{color:"white",
+                    border: "none",backgroundColor:"darkRed",padding:"10px"}}
+                    onClick={this.handleStore}>
+                    Add Item</button>
+                 </div>
+                 <div id="container">
                  <Box title="Tools" items={this.state.childlist[0]}></Box>
                  <Box title="Technologies" items={this.state.childlist[1]}></Box>
                  <Box title="Databases" items={this.state.childlist[2]}></Box>
-                 
+                 </div>
+                
                  
           </div>
                
         );
+     }
+
+     componentDidMount(){
+       console.log("Component mounted");
      }
 }
 
--- src/components/second.js
+++ src/components/second.js
@@ -2,9 +2,17 @@ import React from 'react';
 import './second.css';
 
 class Second extends React.Component{
+    constructor(props){
+        super(props);
+        console.log("Second component(box) Constructor")
+    }
+    componentWillMount(){
+        console.log("Component about to be rendered(box)");
+    }
     render(){
-        console.log(this.props.items);
-        let code=this.props.items.map(x=><li>{x}</li>);
+        console.log("box component rendering")
+        
+        let code=this.props.items.map(x=><li key={x}>{x}</li>);
         return(
             <div className="box">
                 <h3>{this.props.title}</h3>
