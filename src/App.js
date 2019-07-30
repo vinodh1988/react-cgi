ce0fc4fbb6ac18ec1bbec81a913e89dfb4268418
vinodh1988 <k.c.vinodh@hotmail.com>
2019-07-30 16:53:55 +0530

    Map Commit

--- src/App.js
+++ src/App.js
@@ -5,12 +5,22 @@ import './App.css';
 
 class App extends React.Component{
      render(){
+       let appname="ReactJS application- ABCSoft";
+       let developers=["Rahul","Ramu","Kiran","Johnson"];
+      let code= developers.map(x=><li>{x}</li>)
         return(
           <div>
                  <h1> React JS component is working...!!!! </h1>
+                 <h4> {appname}</h4>
+                 <h4><u>Developers</u></h4>
+                 <ul>
+                   {code}
+                 </ul>
                  <Box></Box>
                  <Box></Box>
                  <Box></Box>
+                 
+                 
           </div>
                
         );
