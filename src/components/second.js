57dd3f1f05424128efaf9e962667e3de80d4ba6d
vinodh1988 <k.c.vinodh@hotmail.com>
2019-07-30 16:19:46 +0530

    First Commit

---
+++ src/components/second.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './second.css';
+
+class Second extends React.Component{
+    render(){
+        return(
+            <div className="box">B O X Component </div>
+        )
+    }
+}
+
+export default Second;
